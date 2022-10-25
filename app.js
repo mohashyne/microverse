fc967818feea64ca44b3ca849a1ccdf3edaccfc2
Muhammad Aminu Salihu <mohashyne@gmail.com>
2022-10-25 14:44:49 +0100

    Level 2 lessons completed.

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
-//What arrays are and why they are useful
-// array is just like a variable but it can contain many different values.
-
-let arr = ["apple", "orange", "banana", 77, 95, 101];
-
-console.log(arr);
-//result apple, orange, banana, 77, 95, 101
-console.log(arr[0]); // show the first index of an arr
-//result 'apple' which is index of 1
-
-//index of arrays starts from '0' => index '0' represents the first value.
+//Array operations
 
 
-//Arrays are used to organize values in a specific order
-//they allow us to check values one-by-one and perform tasks with them, using a loop.
-
-let greeting = "Hello my name is";
-let myName = "Muhammad";
- myName += " Salihu"
-let str = "And i have";
-let apples = 22;
-
-console.log(greeting, myName, str, apples);
+let arr = ["apples", "oranges", "banana", 77, 95, 101]
